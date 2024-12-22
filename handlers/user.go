59a9a0ba1af6c3bea0c386d21fc056b2a2fe56b9
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 08:31:16 -0400

    feat: create VerifiyPassword function to verify hashedPassword and password

--- handlers/user.go
+++ handlers/user.go
@@ -13,3 +13,7 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}

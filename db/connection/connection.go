b06df6a6204e37f38398b92d71233876c821145c
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 19:54:10 -0400

    refactor: create package migration and connection

---
+++ db/connection/connection.go
@@ -0,0 +1,37 @@
+package connection
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	dbHost     = os.Getenv("DB_HOST")
+	dbPort     = os.Getenv("DB_PORT")
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("POSTGRES_PASSWORD")
+	dbName     = os.Getenv("DB_NAME")
+	dbTimeZone = os.Getenv("POSTGRES_TIME_ZONE")
+)
+
+func OpenConnection() (*gorm.DB, error) {
+	connectionInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
+		dbHost,
+		dbPort,
+		dbUser,
+		dbPassword,
+		dbName,
+		dbTimeZone)
+
+	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Sucessfuly connected to the database!")
+	return db, nil
+}

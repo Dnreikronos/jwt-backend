635d704e7c69358b0e05d9be3bcb4fd42020465a
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 08:29:46 -0400

    feat: create function for hash user password

---
+++ handlers/user.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"bytes"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}

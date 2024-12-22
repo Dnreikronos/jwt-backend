0d1553ecbe76eb1459c6cbbb4c658a19aa379cfb
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 08:47:03 -0400

    feat: create function for create user with hashed password

--- handlers/user.go
+++ handlers/user.go
@@ -2,12 +2,15 @@ package handlers
 
 import (
 	"bytes"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/Dnreikronos/jwt-backend/models"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
@@ -24,9 +27,30 @@ func VerifyPassword(hashedPassword, password string) error {
 func GenerateToken(user models.User) (string, error) {
 	expirationTime := time.Now().Add(24 * time.Hour)
 	claims := &jwt.StandardClaims{
-		Subject: user.ID.String(),
+		Subject:   user.ID.String(),
 		ExpiresAt: expirationTime.Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtSecret)
+}
+
+func CreateUserHandler(c *gin.Context) {
+	var input models.SignInInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	hashedPassword, err := HashPassword(input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+	newUser := models.User{
+		Email:    input.Email,
+		Password: hashedPassword,
+	}
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Create(&newUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+	}
+	c.JSON(http.StatusCreated, models.FilteredResponse(newUser))
 }

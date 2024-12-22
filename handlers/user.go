b66d02f21b10937d00f14ca72c30fcfea417a864
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 08:40:24 -0400

    feat: create function for genereate jwt token

--- handlers/user.go
+++ handlers/user.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"bytes"
 	"os"
+	"time"
 
+	"github.com/Dnreikronos/jwt-backend/models"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -16,4 +19,14 @@ func HashPassword(password string) (string, error) {
 
 func VerifyPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func GenerateToken(user models.User) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &jwt.StandardClaims{
+		Subject: user.ID.String(),
+		ExpiresAt: expirationTime.Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
 }

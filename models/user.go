008f7eab634a623b3d7eeb67437b67e3dfabebc4
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 19:57:05 -0400

    feat: add password in user struct

--- models/user.go
+++ models/user.go
@@ -5,11 +5,11 @@ import (
 	"gorm.io/gorm"
 )
 
-
-type User struct{
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+type User struct {
+	ID       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
 }
 
 type SignInInput struct {

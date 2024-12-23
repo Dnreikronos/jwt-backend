a8ead6b20db41ca1083ee28d5dda06e49dade9db
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 20:14:46 -0400

    feat: create user test

--- tests/user_tests.go
+++ tests/user_tests.go
@@ -1,9 +1,16 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
 	"github.com/Dnreikronos/jwt-backend/handlers"
 	"github.com/Dnreikronos/jwt-backend/models"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -20,4 +27,23 @@ func setupRouter() (*gin.Engine, *gorm.DB) {
 	router.POST("/login", handlers.LoginHandler)
 	router.GET("/profile", handlers.AuthMiddleware(), handlers.ProfileHandler)
 	return router, db
+}
+
+func TestCreateUserHandler(t *testing.T) {
+	router, db := setupRouter()
+	defer db.Exec("DROP TABLE users")
+
+	userInput := models.SignInInput{Email: "test@example.com", Password: "password123"}
+	body, _ := json.Marshal(userInput)
+
+	req, _ := http.NewRequest("POST", "/create", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	var user models.User
+	db.Where("email = ?", userInput.Email).First(&user)
+	assert.NotEmpty(t, user.ID)
+	assert.True(t, user.Verified)
 }

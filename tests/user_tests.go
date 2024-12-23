67f29ae8e60e806dda85ef1854eeb0ee89ae0f94
Dnreikronos <dnreikronos2@gmail.com>
2024-12-22 20:05:42 -0400

    refactor: adjust identation

--- tests/user_tests.go
+++ tests/user_tests.go
@@ -8,11 +8,9 @@ import (
 	"gorm.io/gorm"
 )
 
-
 func setupRouter() (*gin.Engine, *gorm.DB) {
 	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
 	db.AutoMigrate(&models.User{})
-
 
 	router := gin.Default()
 	router.Use(func(c *gin.Context) {
